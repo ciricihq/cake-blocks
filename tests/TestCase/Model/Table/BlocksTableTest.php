652e3f286b26beaebae456cedaca98ec2db03a7c
Òscar Casajuana <elboletaire@underave.net>
2016-04-13 21:06:05 +0200

    Added missing BlocksTableClass

---
+++ tests/TestCase/Model/Table/BlocksTableTest.php
@@ -0,0 +1,99 @@
+<?php
+namespace Cirici\Blocks\Test\TestCase\Model\Table;
+
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+use Cirici\Blocks\Model\Table\BlocksTable;
+
+/**
+ * BlocksTable Test Case
+ *
+ * @coversDefaultClass \Cirici\Blocks\Model\Table\BlocksTable
+ */
+class BlocksTableTest extends TestCase
+{
+    /**
+     * Test subject
+     *
+     * @var \Cirici\Blocks\Model\Table\BlocksTable
+     */
+    public $Blocks;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.cirici/blocks.bl_blocks'
+    ];
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $config = TableRegistry::exists('Blocks') ? [] : ['className' => 'Cirici\Blocks\Model\Table\BlocksTable'];
+        $this->Blocks = TableRegistry::get('Blocks', $config);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Blocks);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initialize method
+     *
+     * @covers ::initialize
+     * @return void
+     */
+    public function testInitialize()
+    {
+        $this->assertTrue($this->Blocks->hasBehavior('Timestamp'));
+    }
+
+    /**
+     * Test validationDefault method
+     *
+     * @covers ::initialize
+     * @covers ::validationDefault
+     * @return void
+     */
+    public function testValidationDefault()
+    {
+        $block = [
+            'title' => 'El títol del bloc',
+            'slug' => 'el-titol-del-bloc',
+            'content' => '<h1>Títol</h1><p>Contingut</p>'
+        ];
+        $block = $this->Blocks->newEntity($block);
+        $this->assertTrue(empty($block->errors()));
+
+        $block = [
+            'title' => null,
+            'slug' => 'el-titol-del-bloc',
+            'content' => '<h1>Títol</h1><p>Contingut</p>'
+        ];
+        $block = $this->Blocks->newEntity($block);
+        $this->assertFalse(empty($block->errors()));
+
+        $block = [
+            'title' => 'El títol del bloc',
+            'slug' => null,
+            'content' => '<h1>Títol</h1><p>Contingut</p>'
+        ];
+        $block = $this->Blocks->newEntity($block);
+        $this->assertFalse(empty($block->errors()));
+    }
+}
